677a728ad10416eaa6fd480f4cc7a6c6dbab4187
Hudson Cooper <hncooper96@gmail.com>
2024-12-30 12:21:33 -0800

    vec

--- src/ast.rs
+++ src/ast.rs
@@ -1,14 +1,16 @@
+use std::iter;
+
 // Enum to represent regex types
 #[derive(Debug, Clone)]
 pub enum Regex {
-    Empty,                          // Matches nothing
-    Epsilon,                        // Matches the empty string
-    Literal(char),                  // Matches a specific character
-    Concat(Box<Regex>, Box<Regex>), // Concatenation
-    Or(Box<Regex>, Box<Regex>),     // Union (alternation)
-    And(Box<Regex>, Box<Regex>),    // Intersection
-    Not(Box<Regex>),                // Negation
-    Star(Box<Regex>),               // Kleene star
+    Empty,              // Matches nothing
+    Epsilon,            // Matches the empty string
+    Literal(char),      // Matches a specific character
+    Concat(Vec<Regex>), // Concatenation
+    Or(Vec<Regex>),     // Union (alternation)
+    And(Vec<Regex>),    // Intersection
+    Not(Box<Regex>),    // Negation
+    Star(Box<Regex>),   // Kleene star
     Remainder {
         // Value mod divisor is target_remainder
         divisor: u32,
@@ -23,9 +25,9 @@ impl Regex {
             Regex::Empty => false,
             Regex::Epsilon => true,
             Regex::Literal(_) => false,
-            Regex::Concat(r, s) => r.nullable() && s.nullable(),
-            Regex::Or(r, s) => r.nullable() || s.nullable(),
-            Regex::And(r, s) => r.nullable() && s.nullable(),
+            Regex::Concat(rxs) => rxs.iter().all(|rx| rx.nullable()),
+            Regex::Or(rxs) => rxs.iter().any(|rx| rx.nullable()),
+            Regex::And(rxs) => rxs.iter().all(|rx| rx.nullable()),
             Regex::Not(r) => !r.nullable(),
             Regex::Star(_) => true,
             Regex::Remainder {
@@ -55,17 +57,26 @@ impl Regex {
                     Regex::Empty
                 }
             }
-            // ∂a (r · s) = ∂a r · s + ν(r) · ∂a s
-            Regex::Concat(r, s) => Regex::Or(
-                Box::new(Regex::Concat(Box::new(r.derivative(c)), s.clone())),
-                Box::new(Regex::Concat(Box::new(r.null()), Box::new(s.derivative(c)))),
-            ),
-            Regex::Or(r, s) => Regex::Or(Box::new(r.derivative(c)), Box::new(s.derivative(c))),
-            Regex::And(r, s) => Regex::And(Box::new(r.derivative(c)), Box::new(s.derivative(c))),
-            Regex::Not(r) => Regex::Not(Box::new(r.derivative(c))),
-            Regex::Star(r) => {
-                Regex::Concat(Box::new(r.derivative(c)), Box::new(Regex::Star(r.clone())))
+            // ∂_c (r · s) = ∂_c r · s + ν(r) · ∂_c s
+            Regex::Concat(rxs) => {
+                fn deriv_concat(dr: Regex, vr: Regex, s: &Regex, c: &char) -> Regex {
+                    Regex::Or(vec![
+                        Regex::Concat(vec![dr, s.clone()]),
+                        Regex::Concat(vec![vr, s.derivative(c)]),
+                    ])
+                }
+                let mut deriv_so_far = Regex::Empty;
+                let mut null_so_far = Regex::Epsilon;
+                for s in rxs.iter() {
+                    deriv_so_far = deriv_concat(deriv_so_far, null_so_far.clone(), s, c);
+                    null_so_far = Regex::Concat(vec![null_so_far, s.null()]);
+                }
+                deriv_so_far
             }
+            Regex::Or(rxs) => Regex::Or(rxs.iter().map(|r| r.derivative(c)).collect()),
+            Regex::And(rxs) => Regex::And(rxs.iter().map(|r| r.derivative(c)).collect()),
+            Regex::Not(r) => Regex::Not(Box::new(r.derivative(c))),
+            Regex::Star(r) => Regex::Concat(vec![r.derivative(c), Regex::Star(r.clone())]),
             Regex::Remainder {
                 divisor,
                 current_remainder,
@@ -92,9 +103,9 @@ impl Regex {
             Regex::Empty => true,
             Regex::Epsilon => false,
             Regex::Literal(_) => false,
-            Regex::Concat(r, _) => r.empty(),
-            Regex::Or(r, s) => r.empty() && s.empty(),
-            Regex::And(r, s) => r.empty() || s.empty(),
+            Regex::Concat(rxs) => rxs.iter().any(|rx| rx.empty()),
+            Regex::Or(rxs) => rxs.iter().all(|rx| rx.empty()),
+            Regex::And(rxs) => rxs.iter().any(|rx| rx.empty()),
             Regex::Not(r) => !r.empty(),
             Regex::Star(_) => false,
             Regex::Remainder { .. } => false,
@@ -115,29 +126,26 @@ impl Regex {
 
     // Highly suboptimal implementation of the repeat operator
     pub fn repeat(r: Regex, low: usize, high: Option<usize>) -> Regex {
-        let mut result = Regex::Epsilon;
+        let mut result = vec![];
         for _ in 0..low {
-            result = Regex::Concat(Box::new(result), Box::new(r.clone()));
+            result.push(r.clone());
         }
         if let Some(high) = high {
             for _ in low..high {
-                result = Regex::Concat(
-                    Box::new(result),
-                    Box::new(Regex::Or(Box::new(r.clone()), Box::new(Regex::Epsilon))),
-                );
+                result.push(Regex::Or(vec![Regex::Epsilon, r.clone()]));
             }
         } else {
-            result = Regex::Concat(Box::new(result), Box::new(Regex::Star(Box::new(r.clone()))));
+            result.push(Regex::Star(Box::new(r)));
         }
-        result
+        Regex::Concat(result)
     }
 
     pub fn literal(s: &str) -> Regex {
-        let mut result = Regex::Epsilon;
+        let mut result = vec![];
         for c in s.chars() {
-            result = Regex::Concat(Box::new(result), Box::new(Regex::Literal(c)));
+            result.push(Regex::Literal(c));
         }
-        result
+        Regex::Concat(result)
     }
 
     pub fn remainder(divisor: u32, remainder: u32) -> Regex {
@@ -145,7 +153,35 @@ impl Regex {
             divisor,
             current_remainder: 0,
             target_remainder: remainder,
+            scale: 0,
+            fractional_mode: false,
         }
+    }
+}
+
+fn scale_divisor(divisor: f32) -> Result<(u32, u32), String> {
+    if divisor.fract() == 0.0 {
+        Ok((divisor.abs() as u32, 0))
+    } else {
+        let divisor_str = divisor.to_string();
+        let decimal_part = divisor_str
+            .split('.')
+            .nth(1)
+            .ok_or("No decimal part found")?;
+        let scale_power = decimal_part.len();
+
+        if scale_power > 9 {
+            return Err("Too many decimal places, potential u32 overflow".to_string());
+        }
+
+        let scale = 10_u32.pow(scale_power as u32);
+        let scaled_divisor = (divisor * scale as f32).round();
+
+        if scaled_divisor > u32::MAX as f32 {
+            return Err("Scaled divisor exceeds u32::MAX".to_string());
+        }
+
+        Ok((scaled_divisor.abs() as u32, scale))
     }
 }
 
@@ -155,10 +191,10 @@ mod test {
 
     #[test]
     fn test_regex() {
-        let regex = Regex::Concat(
-            Box::new(Regex::Literal('a')),
-            Box::new(Regex::Star(Box::new(Regex::Literal('b')))),
-        );
+        let regex = Regex::Concat(vec![
+            Regex::Literal('a'),
+            Regex::Star(Box::new(Regex::Literal('b'))),
+        ]);
 
         assert_eq!(regex.matches("a"), true);
         assert_eq!(regex.matches("ab"), true);
